229c33b798397e9e2f379c20df52b767e60d6633
Marcus Jiong Kai, Ong <eatmarcus@gmail.com>
2021-01-10 00:11:37 +0800

    added yamseng track

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Box, Image, TextInput, Button } from "grommet";
 import bgOne from '../assets/IMG_134.JPG';
 import { NavBar } from '../components';
@@ -6,15 +6,32 @@ import { NavBar } from '../components';
 class Homepage extends React.Component {
     constructor(props){
         super(props);
-
         this.state={
-            rsvpCode: ""
+            rsvpCode: "",
+            currentSongUrl: "/yam.mp3"
         }
+
+        // this.yam = new Audio("/yam.mp3");
+        // this.seng = new Audio("/seng.mp3")
     }
 
     onInputChange = (e) =>{
         this.setState({
             [e.target.id]: e.target.value.toUpperCase()
+        })
+    }
+
+    onPress = () => {
+        this.setState({currentSongUrl: "/yam.mp3"}, () =>{
+            this.audioRef.play();
+        })
+    }
+
+    onRelease = () => {
+        this.audioRef.pause();
+        this.setState({currentSongUrl: "/seng.mp3"}, ()=>{
+            this.audioRef.load();
+            this.audioRef.play();
         })
     }
 
@@ -33,7 +50,7 @@ class Homepage extends React.Component {
                         <span className="Header"><strong>WEDDING CELEBRATIONS</strong></span>
                         <span className="Header-date"><strong>01/11/2020</strong></span> */}
                         <span className="Header">The Event</span>
-                        <div className="rsvp-form">
+                        {/* <div className="rsvp-form">
                             <span className="Header-rsvp">RSVP Code:</span>
                             <TextInput 
                                 id="rsvpCode" 
@@ -50,6 +67,10 @@ class Homepage extends React.Component {
                                     //submit
                                     alert(this.state.rsvpCode);
                                 }}/>
+                        </div> */}
+                        <div className="rsvp-form" id="yamseng" onPointerDown={this.onPress} onPointerUp={this.onRelease}>
+                            <span className="Header-rsvp">Press to YAM SENG</span>
+                            <audio ref={(input) => {this.audioRef = input}} src={this.state.currentSongUrl} style={{ display: 'none' }} />
                         </div>
                     </Box>
                 </div>
